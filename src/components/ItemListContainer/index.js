b7f348d408b6fcad36ff3715fb4cf2fa3837dab8
KevinPlucci <kmplucci9911@gmail.com>
2022-09-09 22:09:21 -0300

    Carrito agregado

---
+++ src/components/ItemListContainer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "../Title";
+import CustomizedBadges from '../CartWidget';
+
+
+
+export const ItemListContainer = (props) => {
+    return ( 
+        <CustomizedBadges/>
+     );
+}
+ 
+export default ItemListContainer;
--- src/layouts/navbar.js
+++ src/layouts/navbar.js
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from "react-bootstrap"
 import { Outlet, Link } from "react-router-dom"
+import CartWidget from "../components/CartWidget"
 
 const NavBarExample = () => {
     return(
@@ -11,8 +12,8 @@ const NavBarExample = () => {
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
                 <Nav.Link as={Link} to="/" >Home</Nav.Link>
-                <Nav.Link as={Link} to="/about">About</Nav.Link>
-                <Nav.Link as={Link} to="/contact">Contact</Nav.Link>                
+                <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                <Nav.Link as={Link} to="/CartWidget"><CartWidget/></Nav.Link>                               
             </Nav>
             </Navbar.Collapse>
         </Container>
