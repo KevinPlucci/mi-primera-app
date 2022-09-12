11b52c7231fa4c263909c035e36a8d10f0f8c5df
KevinPlucci <kmplucci9911@gmail.com>
2022-09-12 18:39:42 -0300

    Solo falta agregar el problema del nan

--- src/components/ItemListContainer/index.js
+++ src/components/ItemListContainer/index.js
@@ -1,12 +1,23 @@
-import React from "react";
-import Title from "../Title";
+import ItemCount from '../ItemCount/index';
+import React from 'react';
+import Title from '../Title';
 import CustomizedBadges from '../CartWidget';
 
 
 
-export const ItemListContainer = (props) => {
+export const ItemListContainer = ({texto}) => {
+    
+    const onAdd = (quantity) => {
+        console.log(`Compraste ${quantity} unidades`);
+    }
+
+    
     return ( 
-        <CustomizedBadges/>
+        <>
+            <CustomizedBadges/>
+            <Title greeting={texto}/>
+            <ItemCount initial={3} stock={5} onAdd={onAdd}/>
+        </>
      );
 }
  
