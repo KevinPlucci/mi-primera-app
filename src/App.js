ee7f13c55e0a62ef79c1e88db63685cad4f20c4d
KevinPlucci <kmplucci9911@gmail.com>
2022-09-27 16:37:13 -0300

    Primera entrega del proyecto final arreglado!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // PARA IMPORTAR EL ENRUTADO
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // PARA IMPORTAR EL ENRUTADO
 
 import './App.css';
 import ItemListContainer from '../src/component/ItemListContainer';
@@ -10,35 +10,19 @@ import Cart from './component/Cart';
 function App (){
 
     return(
-      <Router> 
-
-     <div className = "root"> 
-
-       <h1>Game Store</h1>
-
-      <NavBar/> {/* Componente menu */}
-
-      <Routes> 
-
-        <Route exact path = '/'> 
-      <ItemListContainer greeting = "Hola Soy un ItemListContainer "/> 
-      </Route>
-
-      <Route exact path='/consola/:idConsola'>
-        <ItemListContainer/>
-      </Route>
-
-      
-
-      <Route exact path='/consola/:idConsola'>
-        <ItemListContainer/>
-      </Route>
-      <Route exact path = '/item/:idItem' component = {ItemDetailContainer}/> {/* SEGUNDA FORMA PARA DEFINIR EL ENRUTADO */}
-      <Route exact path = '/cart' component = {Cart} />
-      </Routes>
-     </div>
-
-     </Router>
+      <BrowserRouter>
+      <div className="root">
+        <h1>Game Store</h1>
+        <NavBar/> { /* Componente menu */}
+      </div>
+        <Routes>
+          <Route exact path="/" element={<ItemListContainer greeting = "Hola Soy un ItemList Container"/>}/>
+          <Route exact path="/consola/:idConsola" element={<ItemListContainer/>}/>
+          <Route exact path="/consola/:idConsola" element = {<ItemListContainer/>}/>
+          <Route exact path = "/item/:idItem" element = {<ItemDetailContainer/>}/>
+          <Route exact path = "/cart" element ={<Cart/>}/>
+        </Routes>
+     </BrowserRouter>
     );
   }
 
